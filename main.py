c7a3d1798bd3824d2bba1bbcb9436561736c8a33
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-05-05 12:45:57 +0530

    Initial commit: comment fetching + cleaning

---
+++ main.py
@@ -0,0 +1,50 @@
+
+
+import os
+from dotenv import load_dotenv
+from cleaner import clean_comments
+from googleapiclient.discovery import build
+
+load_dotenv()
+API_KEY = os.getenv("YOUTUBE_API_KEY")
+VIDEO_ID = "K8AJqCCpL6w"
+
+if not API_KEY:
+    raise ValueError("API key not found. Check your .env file.")
+
+
+API_KEY = os.getenv("YOUTUBE_API_KEY")
+VIDEO_ID = "K8AJqCCpL6w"
+
+def get_comments(video_id,max_comments=70):
+    youtube=build("youtube","v3",developerKey=API_KEY)
+
+    comments=[]
+    request=youtube.commentThreads().list(
+        part="snippet",
+        videoId=video_id,
+        maxResults=100
+    )
+    while request and len(comments)<max_comments:
+        response=request.execute()
+
+        for item in response["items"]:
+           snippet = item["snippet"]["topLevelComment"]["snippet"]
+           comments.append({
+           "text": snippet["textDisplay"],
+           "likes": snippet["likeCount"],
+            "author": snippet["authorDisplayName"],
+})
+           if len(comments)>=max_comments:
+                break
+        request=youtube.commentThreads().list_next(request,response)
+    return comments
+
+
+
+if __name__ == "__main__":
+    comments = get_comments(VIDEO_ID, max_comments=50)
+    cleaned = clean_comments(comments)
+    print(f"\n--- {len(comments)} fetched → {len(cleaned)} after cleaning ---\n")
+    for i, c in enumerate(cleaned, 1):
+        print(f"{i}. [{c['likes']} likes] {c['text'][:100]}\n")
