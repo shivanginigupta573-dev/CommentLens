58f6d8e88965aa6a2a180af6c7fa216813973d1a
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-05-14 23:25:59 +0530

    debugging cors problem

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CORS_ALLOWED_ORIGINS = [
-    "https://comment-lens-alpha.vercel.app",
-]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
+else:
+    CORS_ALLOWED_ORIGINS = [
+        "https://comment-lens-alpha.vercel.app",
+        "http://localhost:3000",
+    ]
