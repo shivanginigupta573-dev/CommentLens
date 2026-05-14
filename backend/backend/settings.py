ab506725b2dc39b7af1633b45fd96aa129b26aba
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-05-14 23:42:48 +0530

    trying to fix cors problem

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,3 +131,23 @@ else:
         "https://comment-lens-alpha.vercel.app",
         "http://localhost:3000",
     ]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
